358ac655381781891ba3dda23bc4aadc8f437d27
RushinGindra <rhg50@scarletmail.rutgers.edu>
2020-02-16 22:01:31 -0500

    Layers : Deconv, Dilated Conv

--- layers.py
+++ layers.py
@@ -22,13 +22,13 @@ class Convolution2D(nn.module):
         self.conv = nn.Conv2d(input_ch, output_ch, kernel_size, stride, padding)
         self.dropout = nn.Dropout(p=dropout_rate)
 
-        if norm_type=='Ins':
+        if norm_type == "Ins":
             self.norm = nn.InstanceNorm2d(output_ch)
 
-        elif norm_type=='Batch':
+        elif norm_type == "Batch":
             self.norm = nn.BatchNorm2d(output_ch, momentum=0.9)
 
-        self.relu = nn.LeakyReLU(negative_slope = relu_factor)
+        self.relu = nn.LeakyReLU(negative_slope=relu_factor)
 
     def forward(self, input):
 
@@ -43,4 +43,82 @@ class Convolution2D(nn.module):
 
         return output
 
-        
+
+class DilatedConv2D(nn.module):
+    def __init__(self,
+                input_ch, output_ch,
+                kernel_size=3,
+                rate=2,
+                padding_mode="VALID",
+                dropout_rate=None,
+                norm_type=None,
+                do_relu=True, relu_factor=0,
+                is_training=True):
+
+        super(Convolution2D, self).__init__()
+
+        self.do_relu = do_relu
+        self.norm_type = norm_type
+
+        padding = 0 if padding_mode="VALID" else 1
+
+        self.dil_conv = nn.Conv2d(input_ch, output_ch, kernel_size, dilation=rate, padding)
+        self.dropout = nn.Dropout(p=dropout_rate)
+
+        if norm_type == "Ins":
+            self.norm = nn.InstanceNorm2d(output_ch)
+
+        elif norm_type == "Batch":
+            self.norm = nn.BatchNorm2d(output_ch, momentum=0.9)
+
+        self.relu = nn.LeakyReLU(negative_slope=relu_factor)
+
+    def forward(self, input):
+
+        output = self.dil_conv(input)
+        output = self.dropout(output)
+
+        if not self.norm_type is None:
+            output = self.norm(output)
+
+        if self.do_relu:
+            output = self.relu(output)
+
+        return output
+
+
+class Deconvolution2D(nn.module):
+    def __init__(self,
+                input_ch, output_ch,
+                kernel_size=3, stride=1,
+                padding_mode="VALID",
+                norm_type=None,
+                do_relu=True, relu_factor=0,
+                is_training=True):
+
+        self.do_relu = do_relu
+        self.norm_type = norm_type
+
+        padding = 0 if padding_mode = "VALID" else 1
+
+        self.deconv = nn.ConvTranspose2d(input_ch, output_ch, kernel_size, stride, padding)
+
+        if norm_type == "Ins":
+            self.norm = nn.InstanceNorm2d(output_ch)
+
+        elif norm_type == "Batch":
+            self.norm = nn.BatchNorm2d(output_ch, momentum=0.9)
+
+        self.relu = nn.LeakyReLU(negative_slope=relu_factor)
+
+    def forward(self, input):
+
+        output = self.deconv(input)
+
+        if not self.norm_type is None:
+            output = self.norm(output)
+
+        if self.do_relu:
+            output = self.relu(output)
+
+        return output
