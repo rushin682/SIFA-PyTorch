94df9f6bf375b9488b2f609030754aecd015fe92
rushin682 <rhg50@scarletmail.rutgers.edu>
2020-02-18 17:10:32 -0500

    Generator S->T : complete. TBT

--- model.py
+++ model.py
@@ -31,26 +31,60 @@ class Generator_S_T(nn.module):
     def __init__(self, skip_conn=False):
         super(Generator_S_T, self).__init__()
 
+        gch = 32 # generator minimum channel multiple
+        self.padding = "CONSTANT"
+
         self.generator = nn.Sequential(OrderedDict([
-                            ('conv1', Convolution2D(input_ch, 32,
+                            ('conv1', Convolution2D(input_ch, gch,
                                                     kernel_size=7, stride=1,
-                                                    norm_type='Ins')),
-                            ('conv2', Convolution2D(32, 32*2,
+                                                    norm_type="Ins")),
+                            ('conv2', Convolution2D(gch, gch*2,
                                                     kernel_size=3, stride=2,
-                                                    norm_type='Ins')),
-                            ('conv3', Convolution2D(32*2, 32*4,
+                                                    norm_type="Ins")),
+                            ('conv3', Convolution2D(gch*2, gch*4,
                                                     kernel_size=3, stride=2,
-                                                    norm_type='Ins')),
+                                                    norm_type="Ins")),
 
-                            ('res_block1', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block2', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block3', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block4', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block5', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block6', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block7', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block8', Residual_block(32*4, 32*4, padding_mode))
-                            ('res_block9', Residual_block(32*4, 32*4, padding_mode))
+                            ('res_block1', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block2', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block3', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block4', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block5', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block6', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block7', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block8', Residual_block(gch*4, gch*4, padding=self.padding)),
+                            ('res_block9', Residual_block(gch*4, gch*4, padding=self.padding)),
 
-                            ('deconv1', Deconvolution2D())
-        ]))
+                            ('deconv1', Deconvolution2D(gch*4, gch*2,
+                                                        kernel_size=3, stride=2,
+                                                        padding_mode="SAME",
+                                                        norm_type='Ins')),
+
+                            ('deconv2', Deconvolution2D(gch*2, gch,
+                                                        kernel_size=3, stride=2,
+                                                        padding_mode="SAME",
+                                                        norm_type='Ins')),
+
+                            ('conv_final', Convolution2D(gch, 1,
+                                                        kernel_size=7, stride=1,
+                                                        padding_mode="SAME",
+                                                        norm_type=None,
+                                                        do_relu=False)),
+                            ]))
+
+        self.tanh = nn.Tanh()
+
+    def forward(self, input):
+
+        #Some padding
+        output = self.generator(padded_input)
+
+        if skip_conn is True:
+            output = self.tanh(input + output)
+
+        else:
+            output = self.tanh(output)
+
+        return output
+
+    
